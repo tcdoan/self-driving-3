d6073c33dae6a20121a0d32c55521f2684886e3c
Thanh Doan <thdoa@microsoft.com>
2020-08-07 04:21:16 -0700

    Filters as a tools to blur, smooth and template matching.

---
+++ computer-vision/image_processing/filters.m
@@ -0,0 +1,109 @@
+# pkg list;
+# pkg load image;
+
+img = imread('dolphin.png');
+imshow(img);
+
+% FUNCTIONS: 
+function result = scale(img, value)
+  result = value .* img;
+endfunction
+dolphin = imread('dolphin.png');
+imshow(scale(dolphin, 1.5));
+
+% Blend two images
+function output = blend(a, b, alpha)
+    output = alpha*a + (1-alpha)*b;
+endfunction
+
+% Test code:
+dolphin = imread('dolphin.png');
+bicycle = imread('bicycle.png');
+
+
+hsize = 17;
+sigma = 5;
+h = fspecial('gaussian', hsize, sigma);
+
+surf(h);
+imagesc(h);
+outim = imfilter(bicycle , h);
+imshow(outim);
+
+% Remove noise with a Gaussian filter
+im = imread('saturn.png');
+imshow(im);
+
+%% Add some noise
+noise_sigma = 25;
+noise = randn(size(im)) .* noise_sigma;
+noisy_im = im + noise;
+imshow(noisy_im);
+
+%% Create a Gaussian filter
+filter_size = 11;
+filter_sigma = 2;
+
+pkg load image;
+ksize = 11;
+sigma = 2;
+
+filter = fspecial('gaussian', ksize, sigma);
+smoothed = imfilter(noisy_im, filter);
+
+subplot(1,2,1), imshow(noisy_im);
+subplot(1,2,2), imshow(smoothed);
+
+
+%% Load an image
+img = imread('fall-leaves.png');  % also available: peppers.png, mandrill.png
+imshow(img);
+
+ksize = 21;
+sigma = 3;
+filter = fspecial('gaussian', ksize, sigma);
+
+% smoothed = imfilter(img, filter, 'replicate');
+smoothed = imfilter(img, filter, 'symmetric');
+subplot(1,2,1), imshow(img);
+subplot(1,2,2), imshow(smoothed);
+
+
+
+% Apply a median filter
+pkg load image;
+
+%% Read an image
+img = imread('moon.png');  % also try: brooklyn-bridge.png, penny-farthing.png
+imshow(img);
+
+%% TODO: Add salt & pepper noise
+noisy_im = imnoise(img, 'salt & pepper', 0.02);
+
+%% TODO: Apply a median filter (how is the result different compared to Gaussian smoothing?)
+median_filter = medfilt2(noisy_im);
+subplot(1,2,1), imshow(noisy_im );
+subplot(1,2,2), imshow(median_filter);
+
+
+% Find template 1D
+% NOTE: Function definition must be the very first piece of code here!
+function index = find_template_1D(t, s)
+  % TODO: Locate template t in signal s and return index
+  % NOTE: Turn off all output from inside the function before submitting!
+  c = normxcorr2(t, s);
+  %disp([1:size(c,2); c]);
+  [maxValue rawIndex] = max(c);
+  index = rawIndex - size(t, 2) + 1; 
+endfunction
+
+pkg load image; % AFTER function definition
+
+% Test code:
+s = [-1 0 0 1 1 1 0 -1 -1 0 1 0 0 -1];
+t = [1 1 0];
+disp('Signal:'), disp([1:size(s, 2); s]);
+disp('Template:'), disp([1:size(t, 2); t]);
+
+index = find_template_1D(t, s);
+disp('Index:'), disp(index);
--- computer-vision/image_processing/images_as_functions.m
+++ computer-vision/image_processing/images_as_functions.m
@@ -1,7 +1,7 @@
 # pkg list;
 # pkg load image;
 
-img = imread('dolphin.jpg');
+img = imread('dolphin.png');
 imshow(img);
 
 
@@ -15,3 +15,93 @@ cropped = img(110:310, 10:160);
 imshow(cropped);
 
 % TODO: Find out cropped image size
+disp(size(cropped))
+
+% Color planes
+img = imread('fruit.png');
+imshow(img);
+disp(size(img));
+
+% TODO: Select a color plane, display it, inspect values from a row
+img_red = img(:, :, 1);
+plot(img_red(150,:));
+img_green = img(:, :, 2);
+img_blue = img(:, :, 3);
+
+imshow(img_red);
+
+imshow(img_green);
+
+imshow(img_blue);
+
+
+dolphin = imread('dolphin.png');
+bicycle = imread('bicycle.png');
+
+average1 =  dolphin / 2 + bicycle / 2;
+average2 =  (dolphin + bicycle) / 2;
+imshow(average1);
+figure, imshow(average2);
+
+subplot(1,2,1), imshow(average1)
+subplot(1,2,2), imshow(average2)
+
+% FUNCTIONS: 
+function result = scale(img, value)
+  result = value .* img;
+endfunction
+dolphin = imread('dolphin.png');
+imshow(scale(dolphin, 1.5));
+
+% Blend two images
+function output = blend(a, b, alpha)
+    output = alpha*a + (1-alpha)*b;
+endfunction
+
+% Test code:
+dolphin = imread('dolphin.png');
+bicycle = imread('bicycle.png');
+
+result = blend(dolphin, bicycle, 0.75);
+imshow(result); % note: will result in an error if blend() returns empty or incorrect value
+
+sigma = 100;
+im = imread('fruit.png');
+noise = randn(size(im)) .* sigma;
+output = im + noise;
+imshow(output);
+
+% Image processing function imabsdiff
+% imabsdiff = (a-b) + (b-a)
+pkg load image;
+imshow(imabsdiff(dolphin, bicycle));
+
+
+noise = randn([1 100]);
+% x is bin centers, n is count of bin values
+[n, x] = hist(noise, [-3 -2 -1 0 1 2 3]);
+disp([x; n]);
+plot(x, n);
+
+noise = randn([1 10000]);
+[n, x] = hist(noise, linspace(-3, 3, 21));
+% disp([x; n]);
+plot(x, n);
+
+% TODO: Try generating other kinds of random numbers.
+height = 3;
+width = 4;
+
+% (3,4) matrix of uniform random values between 0.0 to 1.0
+x = rand(height, width);
+disp(x);
+
+% (3,4) matrix of uniform random values between 0 to 10
+y = randi(10, height, width);
+disp(y);
+
+% A 2D grid of random Gaussian values
+height = 3;
+width = 4;
+x = randn(height, width);
+disp(x);
