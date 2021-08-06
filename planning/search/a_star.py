4ff38a182a643c7104119648d488d801132a7037
An Doan <minh.an.doan@gmail.com>
2021-08-05 22:18:49 -0700

    Implement A* search algorithm.

--- planning/search/a_star.py
+++ planning/search/a_star.py
@@ -39,17 +39,14 @@ def search(grid,init,goal,cost,heuristic):
     # ----------------------------------------
     # modify the code below
     # ----------------------------------------
-    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-    closed[init[0]][init[1]] = 1
-
     expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
-    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
 
-    x = init[0]
-    y = init[1]
-    g = 0
+    x,y = init
+    f = heuristic[x][y]
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    closed[x][y] = 1
 
-    open = [[g, x, y]]
+    open = [[f, x, y]]
 
     found = False  # flag that is set when search is complete
     resign = False # flag set if we can't find expand
@@ -63,13 +60,12 @@ def search(grid,init,goal,cost,heuristic):
             open.sort()
             open.reverse()
             next = open.pop()
-            x = next[1]
-            y = next[2]
-            g = next[0]
+            f,x,y = next
+            g = f - heuristic[x][y]
             expand[x][y] = count
             count += 1
             
-            if x == goal[0] and y == goal[1]:
+            if [x,y] == goal:
                 found = True
             else:
                 for i in range(len(delta)):
@@ -78,7 +74,12 @@ def search(grid,init,goal,cost,heuristic):
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
                             g2 = g + cost
-                            open.append([g2, x2, y2])
+                            f2 = g2+heuristic[x2][y2]
+                            open.append([f2, x2, y2])
                             closed[x2][y2] = 1
 
     return expand
+
+expand = search(grid,init,goal,cost,heuristic)
+for i in range(len(expand)):
+    print(expand[i])
