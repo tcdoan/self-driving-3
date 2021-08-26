3901e68052f80382ffb6f579e1da3ded9be47831
Thanh Doan <doant@outlook.com>
2021-08-25 22:06:50 -0700

    Initial BFS code - starting code for Hybrid A*.

---
+++ planning/trajectory/hybrid_breadth_first.h
@@ -0,0 +1,51 @@
+#ifndef HYBRID_BREADTH_FIRST_H_
+#define HYBRID_BREADTH_FIRST_H_
+
+#include <vector>
+using std::vector;
+
+class HBF 
+{
+public:
+    HBF();
+    virtual ~HBF();
+
+    struct maze_s {
+        int g;
+        double x;
+        double y;
+        double theta;
+    };
+
+    struct maze_path {
+        vector<vector<vector<int>>> closed;
+        vector<vector<vector<maze_s>>> came_from;
+        maze_s final;
+    };
+
+    int theta_to_stack_number(double theta);
+
+    int idx(double float_num);
+
+    vector<maze_s> expand(const maze_s &state);
+
+    vector<maze_s> reconstruct_path
+    (
+        vector<vector<vector<maze_s>>> &came_from,
+        vector<double> &start, HBF::maze_s &final
+    );
+
+    maze_path search
+    (
+        vector<vector<int>> &grid,
+        vector<double> &start,
+        vector<int> &goal
+    );
+
+private:
+    const int NUM_THETA_CELLS = 90;
+    const double SPEED = 1.45;
+    const double LENGTH = 0.5;
+};
+
+#endif  // HYBRID_BREADTH_FIRST_H_
